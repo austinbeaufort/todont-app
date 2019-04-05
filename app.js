96301e019b87d9585e879b9990e5b3429f43fbe7
Austin Beaufort <tinyhatcoding@gmail.com>
2019-04-05 11:37:10 -0400

    Version 1.0.1 , added local storage and 'clear all' button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-listContainer = document.querySelector('.list');
-todoInput = document.querySelector('.enter-item');
+let listContainer = document.querySelector('.list');
+let todoInput = document.querySelector('.enter-item');
+let button = document.querySelector('.button');
 
 const inputHandler = e => {
     if(e.keyCode == 13) {
@@ -7,14 +8,25 @@ const inputHandler = e => {
         if (input === '') return;
         displayHandlers.clearDisplay();
         displayHandlers.createItem(input);
+        // put item in local storage
+        localStorage.setItem(input, input);
+        if (localStorage.length >= 2) {
+            button.disabled = false;
+        }
     }
 }
 
+// helps prevent XSS attacks.
+var sanitizeHTML = function (str) {
+	var temp = document.createElement('div');
+	temp.textContent = str;
+	return temp.innerHTML;
+};
 
 const displayHandlers = {
     createItem(item) {
         let newItem = document.createElement('LI');
-        newItem.innerHTML = `<p class="item" >${item}</p> <i class="fas fa-bomb"></i>`;
+        newItem.innerHTML = `<p class="item" style="font-familly: 'Indie Flower', cursive" >${sanitizeHTML(item)}</p> <i class="fas fa-bomb"></i>`;
         newItem.classList.add('list-item');
         listContainer.appendChild(newItem);
     },
@@ -28,6 +40,13 @@ const displayHandlers = {
         // deleting elements
         if (todo.classList.contains('fas')) {
             let li = todo.parentElement;
+            // remove item from local storage
+            let storageValue = li.firstChild.textContent;
+            localStorage.removeItem(storageValue);
+            if (localStorage.length < 2) {
+                button.disabled = true;
+            }
+            // animation
             setTimeout(() => {
                 li.classList.add('heartbeat');
                 setTimeout(() => {
@@ -48,13 +67,36 @@ const displayHandlers = {
             todo.classList.remove('crossout');
         }
     },
-
-
+    
+    clearAllHandler() {
+        localStorage.clear();
+        let lis = document.querySelectorAll('li');
+        lis.forEach(li => {
+            li.classList.add('hide');
+        })
+        button.disabled = true;
+    }
 }
 
+
+// render local storage to screen 
+const storageHandler = () => {
+    window.onload = () => {
+        keys = Object.keys(localStorage);
+        keys.forEach(key => {
+            displayHandlers.createItem(key);
+        });
+        if (localStorage.length >= 2) {
+            button.disabled = false;
+        }
+    }
+}
+storageHandler();
+
+
+// clear all items including local storage
+button.addEventListener('click', displayHandlers.clearAllHandler);
 
 todoInput.addEventListener('keydown', inputHandler);
 listContainer.addEventListener('click', displayHandlers.handleClick);
 
-
-console.log(todoInput, listContainer);
